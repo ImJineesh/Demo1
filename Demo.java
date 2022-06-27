6b44777a75efc23d31ed3344f8b6e01187025515
ImJineesh <Jinesh010894@gmail.com>
2022-06-28 00:29:38 +0530

    Just to check code

---
+++ Demo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Demo
+
+{
+	
+	public static Void main(String [] args) 
+{
+
+	Scanner sc=new Scanner (System.in);
+
+	System.out.print("Enter A Value");
+	int a=sc.nextInt();
+
+	System.out.print("Enter B Value");
+	int b=sc.nextInt();
+
+	int c=a+b;
+
+	System.out.println("A: " + a);
+	System.out.println("B: " + b);
+
+	
+}
+
+	}
---
+++ Emp.java
@@ -0,0 +1,25 @@
+class A
+{
+	A()
+	{
+
+	for(int i=0;i<=10;i++)
+	System.out.println(i);
+
+	}
+
+}
+
+class Emp
+
+{
+
+	public static void main (String [] args)
+{
+
+
+	A a= new A();
+}
+
+
+}
